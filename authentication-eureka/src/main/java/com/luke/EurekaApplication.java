98377b4bfcdb42c863ba98b59607581acaea4473
wshanti <wshanti@163.com>
2020-03-03 14:14:23 +0800

    aaa

---
+++ authentication-eureka/src/main/java/com/luke/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.luke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author anti
+ * @date 2019-08-18 10:31
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
